933bd3d2072f0b6d72e3dc07ea52b3be0a425a0a
Failattu <kalle.sukeltaja@gmail.com>
2015-07-13 20:45:24 +0300

    The very basic tools will be here

---
+++ Tools/GetIPv4.ps1
@@ -0,0 +1,3 @@
+﻿Write-Verbose "Computer ip resolving"
+$computer = gc env:computername
+(Resolve-DnsName $computer).ip4address
---
+++ Tools/SetGroupPolicy.ps1
@@ -0,0 +1,2 @@
+﻿Write-Verbose "Set's group policy that blocks multi user login by remote to allow login"
+Set-ItemProperty 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -Name fSingleSessionPerUser -Value 0x00000000 
